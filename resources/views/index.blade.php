27000c47a97d18838cf2d413bb2d63a15c3401c2
Nobu <86172592+bunon0@users.noreply.github.com>
2023-03-13 13:58:53 +0900

    ✨ base-layoutsの作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,19 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <title>{{ config('app.name') }}</title>
-
-  <!-- Fonts -->
-  <link rel="preconnect" href="https://fonts.bunny.net">
-  <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-</head>
-
-<body>
-
-</body>
-
-</html>
+@section('content')
+  <p class="text-center">HOME画面</p>
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer bg-primary text-white">
+  <div class="container d-flex justify-content-center align-items-center py-2">
+    <p class="m-0">&copy; 2023 {{ config('app.name') }}</p>
+  </div>
+</footer>
