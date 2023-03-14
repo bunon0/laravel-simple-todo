3825f4feb28f2d01cd62e415d1810651f7dd83a5
Nobu <86172592+bunon0@users.noreply.github.com>
2023-03-14 10:09:10 +0900

    ✨ GoalのCREATE機能の追加

--- resources/views/goals/index.blade.php
+++ resources/views/goals/index.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
   <div class="container">
+    {{-- flash Message --}}
+    @if (session('message'))
+      <div class="alert alert-success text-center">
+        {{ session('message') }}
+      </div>
+    @endif
+    {{-- error --}}
+    @if ($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+          @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+          @endforeach
+        </ul>
+      </div>
+    @endif
     <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#goalAddModal">
       <i class="fa-solid fa-square-plus"></i>
       <span>ゴールの追加</span>
@@ -12,6 +28,8 @@
     </button>
   </div>
 
+  {{-- <p>{{ dd($goals) }}</p> --}}
+
   <!-- GoalModal -->
   <div class="modal fade" id="goalAddModal" tabindex="-1" aria-labelledby=goalAddModal" aria-hidden="true">
     <div class="modal-dialog">
@@ -20,9 +38,10 @@
           <h5 class="modal-title" id="goalAddModalLabel">ゴールの追加</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-        <form action="#" method="post">
+        <form action="{{ route('goals.store') }}" method="post">
+          @csrf
           <div class="modal-body">
-            <input type="text" class="form-control" placeholder="目標を入力してください">
+            <input type="text" class="form-control" name="title" maxlength="255" placeholder="目標を入力してください">
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">閉じる</button>
